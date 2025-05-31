7a84a156ce121e1f3df0552b02d36871b2fabdca
harish881 <mailharishdeshpande@gmail.com>
2025-06-01 00:06:21 +0530

    first commit

---
+++ src/main/java/com/harishdeshpande/AliceBakery/AliceBakeryApplication.java
@@ -0,0 +1,13 @@
+package com.harishdeshpande.AliceBakery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AliceBakeryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AliceBakeryApplication.class, args);
+	}
+
+}
